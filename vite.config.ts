bbf43633c21064cf72a951ad5093aa78b8522a72
KarstenKreh <karsten.b.kreh@gmail.com>
2024-11-23 18:08:14 +0100

    fix routing problem when reloading subpages

--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  server: {
-    port: 5173,
-    middlewareMode: false,
-    fs: {
-      strict: true,
-      allow: ['..']
-    }
-  },
+  base: '/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-      },
-      output: {
-        assetFileNames: (assetInfo) => {
-          if (assetInfo.name.endsWith('.ico') || assetInfo.name.endsWith('.png')) {
-            return '[name][extname]'
-          }
-          return 'assets/[name]-[hash][extname]'
-        }
-      }
-    }
   },
-  publicDir: 'public',
 });
